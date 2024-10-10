3938f79b01fe35cc1e442156954db63542fa95e7
kevindandrew <kevindandrew77@gmail.com>
2024-10-10 11:17:04 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+
+
+export default function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/todos' element={<h1>Todos</h1>} />
+        <Route path='/posts' element={<h1>Posts</h1>} />
+        <Route path='/albums' element={<h1>Albums</h1>} />
+      </Routes>
+    </>
+  )
+}
+
+
