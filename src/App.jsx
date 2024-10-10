08f9506fc58f4b7ecb321b8179cbc0a7f9089866
CF-Tuc <carlosfaciano6@gmail.com>
2024-10-10 16:34:34 -0300

    Component created SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-
+import SearchBar from './coponents/SearchBar'
 
 export default function App() {
 
   return (
     <>
-    <div className=' w-full flex justify-between mt-4'>
-        <div className='w-2/4 bg-gray-500 flex h-9 rounded-lg border-black border-spacing-[1px] mx-auto border items-center'>
-          <h2 className='w-1/6 text-white font-bold flex justify-center'>Search</h2>
-          <input className='w-full bg-white border-black/30 rounded-e-lg font-bold ps-2 h-full outline-none'  type="text" placeholder='Search something interesting'/>
-        </div>
-        <h2 className='text-xl font-bold me-5 cursor-pointer'>My album</h2>
-    </div>
-    
+      <SearchBar/>
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
         <Route path='/todos' element={<h1>Todos</h1>} />
