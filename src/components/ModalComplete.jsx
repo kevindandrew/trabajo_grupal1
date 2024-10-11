11c37cf8459cd15bb4e0a58f277295e7199b32b0
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-11 15:57:31 -0500

    ajuste modal

--- src/components/ModalComplete.jsx
+++ src/components/ModalComplete.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function ModalComplete() {
+function ModalComplete({setShowModal}) {
     return (
-        <div className="fixed inset-0 z-50 flex justify-center items-center overflow-y-auto overflow-x-hidden w-full h-[calc(100%-1rem)] max-h-full">
-            <div className="relative p-4 w-full max-w-2xl max-h-full">
-                <div className="relative rounded-lg shadow">
+        <div className="fixed inset-0 z-50 flex justify-center items-center overflow-y-auto overflow-x-hidden w-full h-[calc(100%-1rem)] max-h-full bg-slate-950 bg-opacity-50">
+            <div className="relative p-4 w-full max-w-2xl max-h-full ">
+                <div className="relative rounded-md shadow bg-slate-500">
 
                     <div className="flex items-center justify-center p-4 md:p-5 rounded-t">
                         <h3 className="flex text-center text-xl font-semibold text-black">
@@ -22,7 +22,7 @@ function ModalComplete() {
 
                         <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Delete
                         </button>
-                        <button type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        <button type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center" onClick={setShowModal}>
                             Save
                         </button>
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ModalComplete from './ModalComplete'
 
 
 /* style={{ color: `${task.completed ? '#00ce00' : '#ff0000'}` }} */
 
 export default function Task({ task }) {
+    const [showModal, setShowModal] = useState(false)
+
+    function openModal() {
+        setShowModal(true)
+    }
     return (
-        <button>
-            <div className="flex flex-col justify-between w-64 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow">
-                <span className='text-end' style={{ color: `${task.completed ? '#00ce00' : '#ff0000'}` }}>{task.completed ? 'completed' : 'pending'}</span>
-                <h5 className="text-xl text-center font-bold tracking-tight text-gray-90">{task.title}</h5>
-                <div><p className='hidden'>Texto oculto</p></div>
-            </div>
-        </button>
+        <div>
+            <button>
+                <div className="flex flex-col justify-between w-64 h-64 p-6 bg-white border border-gray-200 rounded-lg shadow" onClick={openModal}>
+                    <span className='text-end' style={{ color: `${task.completed ? '#00ce00' : '#ff0000'}` }}>{task.completed ? 'completed' : 'pending'}</span>
+                    <h5 className="text-xl text-center font-bold tracking-tight text-gray-90">{task.title}</h5>
+                    <div><p className='hidden'>Texto oculto</p></div>
+                </div>
+            </button>
+            {
+                showModal &&
+                <ModalComplete
+                    setShowModal={() => setShowModal(false)}
+                />
+            }
+        </div>
     )
 }
