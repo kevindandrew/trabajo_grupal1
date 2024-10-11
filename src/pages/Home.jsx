cd42bc0e77ec16ce742b3c2a06890d6d50a26ab7
CF-Tuc <carlosfaciano6@gmail.com>
2024-10-11 16:18:26 -0300

    Component created: PostDetails.jsx. It includes the post title and the post body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Album from '../components/Album';
 
 export default function Home() {
   return (
     <section className='w-full h-full bg-orange-100'>
       <div className='flex justify-center w-full h-screen mt-2 gap-x-4 flex-wrap'>
         <div className='w-6/12 bg-black h-2/5 flex justify-center items-center shadow-lg'>
-          <Link to="/albums" className='font-bold text-white cursor-pointer'>Albums</Link>
+          <Link to="./albums" className='font-bold text-white cursor-pointer'>Albums</Link>
         </div>
         <div className='w-2/5 bg-red-300 h-2/5 flex justify-center items-center shadow-lg'>
           <Link to="/posts" className='font-bold text-white cursor-pointer'>Posts</Link>
